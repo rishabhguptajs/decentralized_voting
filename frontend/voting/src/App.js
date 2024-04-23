0ebd92daf75cf145d13c232bec93731e98535ea4
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-04-23 22:53:52 +0530

    contracts folder

--- frontend/voting/src/App.js
+++ frontend/voting/src/App.js
@@ -6,20 +6,20 @@ import Logout from './pages/Logout';
 import Result from './pages/Result';
 import Voting from './pages/Voting-Area';
 import Voter_Register from './pages/Voter-registration';
+import Signup from './pages/Signup';
 import Info from './pages/Info';
 
 function App() {
   return (
     <BrowserRouter>
-    <Sidebar>
     <Routes>
+      <Route path="/"element={<Signup/>}/>
       <Route path="/info"element={<Info/>}/>
       <Route path="/voter-registration"element={<Voter_Register/>}/>
       <Route path="/result"element={<Result/>}/>
       <Route path="/voting-area"element={<Voting/>}/>
       <Route path="/logout"element={<Logout/>}/>
     </Routes>
-    </Sidebar>
     </BrowserRouter>
     
   );
---
+++ frontend/voting/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='w-[100vw] flex flex-row text-center justify-center'>
+      <h1 className=''>Company Name</h1>
+    </div>
+  )
+}
+
+export default Signup
